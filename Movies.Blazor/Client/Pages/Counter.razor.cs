e2db2218e1c249dff532cc5d26b75a5254f6a478
temo14 <temo599922030@gmail.com>
2023-03-03 15:17:29 +0400

    clean up code

--- Movies.Blazor/Client/Pages/Counter.razor.cs
+++ Movies.Blazor/Client/Pages/Counter.razor.cs
@@ -1,23 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
-
-namespace Movies.Blazor.Client.Pages
+﻿namespace Movies.Blazor.Client.Pages
 {
     public partial class Counter
     {
         private int currentCount = 0;
-        private static int currentCountStatic = 0;
-        [CascadingParameter(Name = "Color")] public string Color { get; set; }
         private void IncrementCount()
         {
             currentCount++;
-            currentCountStatic++;
-        }
-
-        [JSInvokable]
-        public static Task<int> GetCurrnetCount()
-        {
-            return Task.FromResult(currentCountStatic);
         }
     }
 }
